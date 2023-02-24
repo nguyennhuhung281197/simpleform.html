0de9ae7071cab65130d5d4731b0699a90282284e
nguyennhuhung281197 <nhuhung281197@gmail.com>
2023-02-24 11:23:51 +0700

    tinh tong diemtb

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let a = parseFloat(prompt("Nhập vào điểm môn vật lý " ) );
+let b = parseFloat(prompt("Nhập vào điểm môn hóa học " ) );
+let c = parseFloat(prompt("Nhập vào điểm môn sinh học " ) );
+let diemTb = ( (a + b + c ) /3 ).toFixed(2);
+
+document.write(diemTb);
+
+
+
+
